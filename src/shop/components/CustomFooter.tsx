233b1e99fd2fb98976d5136561331af8af67fb40
rodri 2 <rodrigomesaw@gmail.com>
2026-03-23 10:59:06 -0300

    Footer_change

--- src/shop/components/CustomFooter.tsx
+++ src/shop/components/CustomFooter.tsx
@@ -6,8 +6,9 @@ export const CustomFooter = () => {
         {/* Footer */}
         <footer className="border-t py-12 px-4 lg:px-8 mt-16">
             <div className="container mx-auto">
-            <div className="border-t mt-8 pt-8 text-center text-sm text-muted-foreground">
+            <div className="border-t mt-4 pt-4 text-center text-sm text-muted-foreground">
                 <p>&copy; {new Date().getFullYear()} <span className="font-bold">CARNICERIA FMP</span>. Todos los derechos reservados.</p>
+                <p className="text-muted-foreground mt-4">Desarrollo - rodrigomesaw@gmail.com</p>
             </div>
             </div>
         </footer>
